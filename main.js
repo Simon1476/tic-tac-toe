35ce133ca7c5ae1c9508f043b2cd0b41abfd361a
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-09-18 11:31:15 +0900

    Initial implementation of Gameboard functions

---
+++ main.js
@@ -0,0 +1,33 @@
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const dropToken = (column, player) => {
+    const availableCells = board
+      .filter((row) => row[column].getValue() === 0)
+      .map((row) => row[column]);
+
+    if (!availableCells.length) return;
+
+    const lowestRow = availableCells.length - 1;
+    board[lowestRow][column].addToken(player);
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+  };
+
+  return { getBoard, dropToken, printBoard };
+}
