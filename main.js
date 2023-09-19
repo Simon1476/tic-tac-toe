b0557491c9b9a7781919a9b0936aefeabac34af2
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-09-19 12:13:45 +0900

    Add checkBoardState Function
    This function checks if there are three consecutive tokens (O or X)
    Add essential logic to determine win or draw status

--- main.js
+++ main.js
@@ -23,7 +23,44 @@ function Gameboard() {
     );
   };
 
-  return { getBoard, dropToken, printBoard };
+  const checkBoardState = () => {
+    // Check Row
+    for (let row = 0; row < 3; row++) {
+      if (
+        board[row][0] === board[row][1] &&
+        board[row][1] === board[row][2] &&
+        board[row][0] !== ""
+      ) {
+        return board[row][0];
+      }
+    }
+
+    // Check Column
+    for (let col = 0; col < 3; col++) {
+      if (
+        board[0][col] === board[1][col] &&
+        board[1][col] === board[2][col] &&
+        board[0][col] !== ""
+      ) {
+        return board[0][col];
+      }
+    }
+
+    // Check diagonal
+    if (
+      (board[0][0] === board[1][1] &&
+        board[1][1] === board[2][2] &&
+        board[0][0] !== "") ||
+      (board[0][2] === board[1][1] &&
+        board[1][1] === board[2][0] &&
+        board[0][2] !== "")
+    ) {
+      return board[1][1];
+    }
+
+    return null; // Return null if no one wins
+  };
+  return { getBoard, dropToken, printBoard, checkBoardState };
 }
 
 function Cell() {
